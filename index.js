c38e0ad179475ca387866cbc587e0376256f720b
Zaid Akhter <zaidakhter1202@gmail.com>
2020-08-30 06:56:56 +0500

    👌 IMPROVE: Create seperate files

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const { ApolloServer, gql } = require("apollo-server-express");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
 
-const app = express();
+const main = () => {
+  const app = express();
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-const resolvers = {
-  Query: {
-    hello: () => "hello",
-  },
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen({ port: 4000 }, () => console.log(`Server ready at http://localhost:4000${server.graphqlPath}`));
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-server.applyMiddleware({ app });
-
-app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+main();
