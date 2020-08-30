b8b8eac9962c01e3090f67e04a4b5152ce3d9ece
Zaid Akhter <zaidakhter1202@gmail.com>
2020-08-30 15:05:32 +0500

    📦 NEW: Define Author and Book types

--- typeDefs.js
+++ typeDefs.js
@@ -1,7 +1,22 @@
 const { gql } = require("apollo-server-express");
 
-module.exports = gql`
+const typeDefs = gql`
+  type Author {
+    id: Int!
+    name: String!
+    email: String!
+    books: [Book]!
+  }
+
+  type Book {
+    id: Int!
+    title: String!
+    author: Author!
+  }
+
   type Query {
     hello: String
   }
 `;
+
+module.exports = typeDefs;
